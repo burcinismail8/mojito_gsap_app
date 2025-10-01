a12ac12e12bd8d1bf59ca4e707eb78dbe1747a4b
burcinismail8 <burcinismail8@gmail.com>
2025-10-01 13:54:11 +0300

    Hero Layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
     <main>
       <NavBar />
+      <Hero />
+      <div className="h-dvh bg-black" />
     </main>
   );
 };
